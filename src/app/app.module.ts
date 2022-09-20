c103a0937bc127afa2481290557006c064f78d32
kishor773 <kishordeshpande310@gmail.com>
2022-09-21 00:24:34 +0530

    fb ui clone toolbar

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
+ import {MatToolbarModule} from '@angular/material/toolbar'; 
+ import {MatGridListModule} from '@angular/material/grid-list';
+ import {MatInputModule} from '@angular/material/input'
+import {TextFieldModule} from '@angular/cdk/text-field';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatInputModule,
+    TextFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
